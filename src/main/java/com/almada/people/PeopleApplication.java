1cab8c12359d98a1a3f53eaa97fa2ba240cc7b9c
celsobritodev <celsinhobrito@gmail.com>
2025-09-13 16:20:39 -0300

    adicionado o banco de dados

--- src/main/java/com/almada/people/PeopleApplication.java
+++ src/main/java/com/almada/people/PeopleApplication.java
@@ -3,6 +3,9 @@ package com.almada.people;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// AULA DO VIDEO: https://youtu.be/VK-RJPl1ALg
+//  COMO FAZER UM CRUD BÁSICO COM SPRING BOOT EM 2023 | JAVA | MAVEN | POSTGRESQL
 @SpringBootApplication
 public class PeopleApplication {
 
